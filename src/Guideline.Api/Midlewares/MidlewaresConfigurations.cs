126de506be0a627186d891186e64776ff79c6e4b
Luciano Robe <lucodifier@gmail.com>
2021-01-28 17:00:29 -0300

    Midlewares configurations

---
+++ src/Guideline.Api/Midlewares/MidlewaresConfigurations.cs
@@ -0,0 +1,58 @@
+﻿using Guideline.Infra.CrossCutting.Ioc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Guideline.Api.Midlewares
+{
+    public static class MidlewaresConfigurations
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "Guideline.Api", Version = "v1" });
+
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                       new string[] { }
+                    }
+                });
+
+            });
+        }
+
+        public static void ConfigureIocDI(this IServiceCollection services)
+        {
+                Bootstrapper.SetupIoC(services);
+        }
+    }
+}
--- src/Guideline.Api/Startup.cs
+++ src/Guideline.Api/Startup.cs
@@ -1,12 +1,10 @@
 using FluentValidation.AspNetCore;
 using Guideline.Api.Midlewares;
-using Guideline.Infra.CrossCutting.Ioc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace Guideline.Api
 {
@@ -25,46 +23,14 @@ namespace Guideline.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
             services.AddCors();
             services.AddHttpContextAccessor();
-
-            // configure DI for application services
-            RegisterServices(services);
-
+            services.ConfigureIocDI();
             services.AddControllers();
-            services.AddSwaggerGen(s =>
-            {
-                s.SwaggerDoc("v1", new OpenApiInfo { Title = "Guideline.Api", Version = "v1" });
-
-                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-
-                s.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                       new string[] { }
-                    }
-                });
-
-            });           
-
+            services.ConfigureSwagger();   
             services.AddMvc()
             .AddFluentValidation(options =>
             {
@@ -72,7 +38,6 @@ namespace Guideline.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -83,15 +48,8 @@ namespace Guideline.Api
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            // global cors policy
-            app.UseCors(x => x
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             // custom jwt auth middleware
@@ -101,11 +59,6 @@ namespace Guideline.Api
             {
                 endpoints.MapControllers();
             });
-        }
-
-        private void RegisterServices(IServiceCollection services)
-        {
-            Bootstrapper.SetupIoC(services);
         }
     }
 }
